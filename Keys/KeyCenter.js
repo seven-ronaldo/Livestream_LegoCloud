b314daecf1386d6af0cd0fe9997552c35e749a44
Code-Apprentice-AI <x00191169@gmail.com>
2024-06-04 16:43:46 +0100

    02

--- Keys/KeyCenter.js
+++ Keys/KeyCenter.js
@@ -1,6 +1,6 @@
 const KeyCenter = {
-    appID: 1170898027 ,
-    appSign: 'a3fe7278f87edf4bd3f07eb799f8283b3ee9f9858dd4403ae48b02d9ae69b833',
+    appID: ,
+    appSign: '',
 }
 
 export default KeyCenter
