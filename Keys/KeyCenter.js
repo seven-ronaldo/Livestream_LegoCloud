303feac42f99aee5e015e5769523c21dfe9d3b63
MSI PC <hao7ehoathanh@gmail.com>
2024-12-01 12:12:11 +0700

    Fix

--- Keys/KeyCenter.js
+++ Keys/KeyCenter.js
@@ -1,6 +1,6 @@
 const KeyCenter = {
-    appID: ,
-    appSign: '',
-}
+  appID: 482908302,
+  appSign: '388f4693f83fd7ab830b919c4f353574fe032fd9cef2ba078e12c31f3c2badda',
+};
 
-export default KeyCenter
+export default KeyCenter;
